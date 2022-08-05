6876c72cfd728cda494c97a57accd189ac8dd9bd
Huzeyfe Yetkiner <yetkinerhuzeyfe@gmail.com>
2022-08-05 23:32:10 +0300

    basic variable

--- javascript/hello-world.js
+++ javascript/hello-world.js
@@ -1,3 +1,40 @@
 //alert("Hello World")
 //document.write('hello world')
 console.log("Hello World")
+document.write('hello afasdworld');
+
+
+let randomVariable //değişkene herhangi bir değer atamadan kullanırsak undefined olarak okunur
+console.log(randomVariable)
+randomVariable = "master"
+console.log(randomVariable)
+
+var randomVariable2 //değişkene herhangi bir değer atamadan kullanırsak undefined olarak okunur
+console.log(randomVariable2)
+randomVariable2 = "master"
+console.log(randomVariable2)
+
+let word = "Huzeyfe Yetkiner"
+console.log(word)
+
+word = "Büşra Yılmaz"
+console.log(word)
+
+
+
+//Değişken ataması yapmadan kullanmaya çalışmak
+//console.log(anotherWord) let ile yazılan değişkeni atama yapmadan kullanmaya kalkarsak hata alırız
+let anotherWord = "erikli"
+console.log(anotherWord)
+
+
+console.log(secondWord)
+var secondWord = "Huzoo"
+console.log(secondWord)
+
+//sabit veriler - const
+const fixedPassword = "159Huzo753"
+console.log(fixedPassword)
+
+//fixedPassword = "753huzo159" yukarıda tanımlanmış const variable program akışında değiştirilemez
+console.log(fixedPassword)
